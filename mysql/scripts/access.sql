6d39a9a2e6f5fb4ed7b2b5743c7e235a99f305bf
victor <victorpsf2@hotmail.com>
2020-08-24 22:23:52 -0300

    incluindo ultimo desenvolvedor no projeto

--- mysql/scripts/access.sql
+++ mysql/scripts/access.sql
@@ -9,3 +9,8 @@ INSERT INTO `Acesso`
 	(`login`, `senha`, `created_at`) 
 VALUES 
 	('42ee70a8d008fbe122072d551ecb9fa36c218401f84922761cb6fa1956141b3e', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2020-07-23 23:19:35');
+
+INSERT INTO `Acesso` 
+	(`login`, `senha`, `created_at`) 
+VALUES 
+	('303a521c440601e46cbb0eecc3f2cf98d49958ffb3aefcb32385992d9d1b5ee5', '8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92', '2020-07-23 23:19:35');
